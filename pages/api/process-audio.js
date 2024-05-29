bd95f2a9139d48c3c178c9a18bb98ab58156b824
Sioaeko <101755125+sioaeko@users.noreply.github.com>
2024-05-29 23:45:51 +0900

    Update process-audio.js

--- pages/api/process-audio.js
+++ pages/api/process-audio.js
@@ -1,5 +1,5 @@
 import { InferenceSession } from 'onnxruntime-node';
-import { processRVC } from '../../server/rvc-model'; // RVC 모델 로드 및 처리 함수 가져오기
+import { processRVC } from '../../server/rvc-model';
 
 let session;
 
@@ -14,21 +14,20 @@ export default async function handler(req, res) {
     await loadModel();
 
     const audioBuffer = req.body.audioBuffer;
+    const settings = req.body.settings;
     if (!audioBuffer) {
       res.status(400).json({ error: 'No audio buffer provided' });
       return;
     }
 
     try {
-      // 기존 모델 처리
       const inputTensor = new Float32Array(audioBuffer);
       const tensor = new session.Tensor('float32', inputTensor, [1, inputTensor.length]);
 
       const results = await session.run({ input: tensor });
       const output = results.output.data;
 
-      // RVC 모델 처리
-      const rvcOutput = await processRVC(audioBuffer);
+      const rvcOutput = await processRVC(audioBuffer, settings);
 
       res.status(200).json({ processedAudio: output, rvcProcessedAudio: rvcOutput });
     } catch (error) {
