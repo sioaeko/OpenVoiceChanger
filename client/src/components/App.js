4253c8b265ef7487bc2035ce23a7854a87c50495
Sioaeko <101755125+sioaeko@users.noreply.github.com>
2024-06-30 17:07:37 +0900

    Create App.js

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VoiceChangerDesktop from './components/VoiceChangerDesktop';
+
+function App() {
+  return (
+    <div className="App">
+      <VoiceChangerDesktop />
+    </div>
+  );
+}
+
+export default App;
