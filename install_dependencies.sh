f2171dcae267519e2f7e30501a6c0553b3185833
Sioaeko <101755125+sioaeko@users.noreply.github.com>
2024-06-30 17:10:40 +0900

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+# Client dependencies
+echo "Installing client dependencies..."
+cd client
 npm install
+cd ..
 
+# Server dependencies
+echo "Installing server dependencies..."
+cd server
+npm install
+cd ..
+
+# Python dependencies
+echo "Installing Python dependencies..."
+cd python_server
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+deactivate
+cd ..
 
 echo "All dependencies installed successfully."
